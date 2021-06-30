b4ce9ed77dc62ef75ac43cacec6684fa2bca9f76
Andre Marquand <amarquand@gmail.com>
2021-06-30 12:09:45 +0200

    added uncertainty to centile curves

--- run_single_nm_updated.py
+++ run_single_nm_updated.py
@@ -37,12 +37,16 @@ idp_ids_rh = df_te.columns.to_list()[90:165]
 idp_ids_sc = df_te.columns.to_list()[165:198]
 idp_ids_glob = df_te.columns.to_list()[208:216] + df_te.columns.to_list()[220:222]
 idp_ids_all = df_te.columns.to_list()[15:207] + df_te.columns.to_list()[208:222]
-idp_ids = ['Right-Hippocampus']
-#idp_ids = idp_ids_lh +idp_ids_glob
+idp_ids = ['lh_S_orbital_med-olfact_thickness']
+#idp_ids = ['lh_S_temporal_sup_thickness']
+#idp_ids = ['avg_thickness']
+#idp_ids = ['lh_S_suborbital_thickness']
+idp_ids = idp_ids_lh + idp_ids_rh + idp_ids_glob
 
 # run switches
 show_plot = True
-force_refit = True
+force_refit = False
+outlier_thresh = 7
 
 # which type of model to run?
 cov_type = 'bspline'  # 'int', 'bspline' or None
@@ -55,10 +59,9 @@ else:
     clr = 'red'
     sex_name = 'female'
 
-# cubic B-spline basis (used for regression)
+# limits for cubic B-spline basis 
 xmin = -5 # boundaries for ages of UKB participants +/- 5
 xmax = 110
-B = create_bspline_basis(xmin, xmax)#, nknots=10)
 
 ################################### RUN #######################################
 
@@ -69,31 +72,12 @@ X0_dummy[:,0] = xx
 X0_dummy[:,1] = sex
 for sid, site in enumerate(cols_site):
     print('configuring dummy data for site',sid, site)
-    # X_dummy = np.zeros((len(xx), len(cols)))
-    # X_dummy[:,0] = xx
-    # X_dummy[:,1] = sex
-    # #X_dummy[:,2] = df_tr['avg_euler_centered_neg_sqrt'].median()
-    # X_dummy[:,sid+len(cols_cov)] = np.ones(len(xx))
-    # # add intercept
-    # X_dummy = np.concatenate((X_dummy, np.ones((len(xx), 1))), axis=1)
-    # # add bspline basis
-    # X_dummy = np.concatenate((X_dummy, np.array([B(i) for i in X_dummy[:,0]])), axis=1)
-
     site_ids = np.zeros((len(xx), len(cols_site)))
     site_ids[:,sid] = 1
     X_dummy = create_design_matrix(X0_dummy, xmin=xmin, xmax=xmax, site_cols=site_ids)
     np.savetxt(os.path.join(data_dir,'cov_bspline_dummy_' + site + '.txt'), X_dummy)
 
-
 print('configuring dummy data for mean')
-# X_dummy = np.zeros((len(xx), len(cols)))
-# X_dummy[:,0] = xx
-# X_dummy[:,1] = sex
-# # add intercept
-# X_dummy = np.concatenate((X_dummy, np.ones((len(xx), 1))), axis=1)
-# # add bspline basis
-# X_dummy = np.concatenate((X_dummy, np.array([B(i) for i in X_dummy[:,0]])), axis=1)
-
 site_ids = np.zeros((len(xx), len(cols_site)))
 X_dummy = create_design_matrix(X0_dummy, xmin=xmin, xmax=xmax, site_cols=site_ids)
 np.savetxt(os.path.join(data_dir,'cov_bspline_dummy_mean.txt'), X_dummy)
@@ -116,49 +100,40 @@ for idp in idp_ids:
     os.makedirs(os.path.join(idp_dir,out_name), exist_ok=True)
     os.chdir(idp_dir)
     
-    # # load data matrices
-    # X_tr = df_tr[cols].to_numpy()
-    # X_te = df_te[cols].to_numpy()
-
-    # # add intercept column 
-    # X_tr = np.concatenate((X_tr, np.ones((X_tr.shape[0],1))), axis=1)
-    # X_te = np.concatenate((X_te, np.ones((X_te.shape[0],1))), axis=1)
+    # configure and save the responses
+    y_tr = df_tr[idp].to_numpy() 
+    y_te = df_te[idp].to_numpy()
     
-    # # create Bspline basis set 
-    # Phi = np.array([B(i) for i in X_tr[:,0]])
-    # Phis = np.array([B(i) for i in X_te[:,0]])
-    # X_tr = np.concatenate((X_tr, Phi), axis=1)
-    # X_te = np.concatenate((X_te, Phis), axis=1)
+    # remove gross outliers
+    yz_tr = (y_tr - np.mean(y_tr)) / np.std(y_tr)
+    yz_te = (y_te - np.mean(y_te)) / np.std(y_te)
+    nz_tr = np.abs(yz_tr) < outlier_thresh
+    nz_te = np.abs(yz_te) < outlier_thresh
+    y_tr = y_tr[nz_tr]
+    y_te = y_te[nz_te]
     
-    X_tr = create_design_matrix(df_tr[cols_cov], site_cols = df_tr[cols_site],
+    resp_file_tr = os.path.join(idp_dir, 'resp_tr.txt')
+    resp_file_te = os.path.join(idp_dir, 'resp_te.txt') 
+    np.savetxt(resp_file_tr, y_tr)
+    np.savetxt(resp_file_te, y_te)
+    
+    y_tr = y_tr[:, np.newaxis]  
+    y_te = y_te[:, np.newaxis]
+    
+    resp_tr_skew = calibration_descriptives(np.loadtxt(resp_file_tr))[0]
+    
+    # configure and save the covariates
+    X_tr = create_design_matrix(df_tr[cols_cov].loc[nz_tr], site_cols = df_tr[cols_site].loc[nz_tr],
                                 basis = 'bspline', xmin = xmin, xmax = xmax)
-    X_te = create_design_matrix(df_te[cols_cov], site_cols = df_te[cols_site],
+    X_te = create_design_matrix(df_te[cols_cov].loc[nz_te], site_cols = df_te[cols_site].loc[nz_te],
                                 basis = 'bspline', xmin = xmin, xmax = xmax)
     
     np.savetxt(os.path.join(idp_dir, 'cov_bspline_tr.txt'), X_tr)
     np.savetxt(os.path.join(idp_dir, 'cov_bspline_te.txt'), X_te)
     
     # configure the covariates to use
-    if cov_type is None:
-        cov_file_tr = os.path.join(idp_dir, 'cov_tr.txt')
-        cov_file_te = os.path.join(idp_dir, 'cov_te.txt')
-    else:
-        cov_file_tr = os.path.join(idp_dir, 'cov_') + cov_type + '_tr.txt'
-        cov_file_te = os.path.join(idp_dir, 'cov_') + cov_type + '_te.txt'
-    resp_file_tr = os.path.join(idp_dir, 'resp_tr.txt')
-    resp_file_te = os.path.join(idp_dir, 'resp_te.txt') 
-    
-    # configure and save the targets
-    y_tr = df_tr[idp].to_numpy() 
-    y_te = df_te[idp].to_numpy()
-    np.savetxt(resp_file_tr, y_tr)
-    np.savetxt(resp_file_te, y_te)
-    
-    y_tr = y_tr[:, np.newaxis]  
-    y_te = y_te[:, np.newaxis]
-    Phi_tr = np.loadtxt(cov_file_tr)
-    
-    resp_tr_skew = calibration_descriptives(np.loadtxt(resp_file_tr))[0]
+    cov_file_tr = os.path.join(idp_dir, 'cov_') + cov_type + '_tr.txt'
+    cov_file_te = os.path.join(idp_dir, 'cov_') + cov_type + '_te.txt'
 
     w_dir = os.path.join(idp_dir, out_name)
     if not force_refit and os.path.exists(os.path.join(w_dir, 'Models', 'NM_0_0_estimate.pkl')):
@@ -172,16 +147,12 @@ for idp in idp_ids:
                      hetero_noise = True)
         else: 
              estimate(cov_file_tr, resp_file_tr, testresp=resp_file_te, 
-                      testcov=cov_file_te, alg='blr', configparam=1,verbose=True,
+                      testcov=cov_file_te, alg='blr', configparam=1,
                       optimizer = 'l-bfgs-b', savemodel=True, standardize = False, 
                       warp=warp, warp_reparam=True) # if verbose true see inbetween estimates 
-        #metrics_new = {'MSLL': 0}
     
     # set up the dummy covariates
-    if cov_type is None:
-        cov_file_dummy = os.path.join(data_dir, 'cov_dummy')
-    else:
-        cov_file_dummy = os.path.join(data_dir, 'cov_' + cov_type + '_dummy')
+    cov_file_dummy = os.path.join(data_dir, 'cov_' + cov_type + '_dummy')
     cov_file_dummy = cov_file_dummy + '_mean.txt'
     
     # make predictions
@@ -232,11 +203,9 @@ for idp in idp_ids:
     for sid, site in enumerate(cols_site):
                 
         # plot the true test data points
-        #idx = np.where(np.bitwise_and(X_te[:,1] == sex, X_te[:,sid+len(cols_cov)] !=0))
         idx = np.where(np.bitwise_and(X_te[:,2] == sex, X_te[:,sid+len(cols_cov)+1] !=0))
     
         # load training data (needed for MSLL)
-        #idx_dummy = np.bitwise_and(X_dummy[:,0] > X_te[idx,0].min(), X_dummy[:,0] < X_te[idx,0].max())
         idx_dummy = np.bitwise_and(X_dummy[:,1] > X_te[idx,1].min(), X_dummy[:,1] < X_te[idx,1].max())
         
         # adjust the intercept
@@ -246,14 +215,12 @@ for idp in idp_ids:
             y_te_rescaled = y_te[idx] - np.median(y_te[idx]) + np.median(med[idx_dummy])
         #y_te_rescaled = y_te[idx]
         if show_plot:
-            #plt.scatter(X_te[idx,0], y_te_rescaled, s=7, color=clr, alpha = 0.1)   
-            plt.scatter(X_te[idx,1], y_te_rescaled, s=7, color=clr, alpha = 0.1)   
+            plt.scatter(X_te[idx,1], y_te_rescaled, s=4, color=clr, alpha = 0.05)   
         y_te_rescaled_all[idx] = y_te_rescaled
     
-    #idx_all = np.where(X_te[:,1] == sex)
-    #sns.jointplot(x=X_te[idx_all,0].ravel(), y=np.log(y_te_rescaled_all[idx_all].ravel()), kind="hex")
-    #plt.hexbin(X_te[idx_all,0].ravel(), y_te_rescaled_all[idx_all].ravel(), cmap=plt.cm.Blues, gridsize=40, alpha=0.5)
-    #sns.kdeplot(x=X_te[idx_all,0].ravel(), y=np.log(y_te_rescaled_all[idx_all].ravel()))
+    #idx_all = np.where(X_te[:,2] == sex)
+    #sns.jointplot(x=X_te[idx_all,1].ravel(), y=np.log(y_te_rescaled_all[idx_all].ravel()), kind="hex")
+    #plt.hexbin(X_te[idx_all,1].ravel(), y_te_rescaled_all[idx_all].ravel(), cmap=plt.cm.Blues, gridsize=40, vmin=0, vmax=50)
     
     if warp is None:
         if show_plot:
@@ -280,30 +247,51 @@ for idp in idp_ids:
         # warp and plot dummy predictions
         med, pr_int = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2), warp_param)
         
-        # plot the centiles
-        junk, pr_int25 = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2), warp_param, percentiles=[0.25,0.75])
-        junk, pr_int95 = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2), warp_param, percentiles=[0.05,0.95])
-        junk, pr_int99 = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2), warp_param, percentiles=[0.01,0.99])
+        beta, junk1, junk2 = nm.blr._parse_hyps(nm.blr.hyp, X_dummy)
+        s2n = 1/beta
+        s2s = s2-s2n
         
+        # plot the centiles
         if show_plot: 
             plt.plot(xx, med, clr)
-            #plt.fill_between(xx, pr_int[:,0], pr_int[:,1], alpha = 0.2,color=clr)
+            # fill the gaps in between the centiles
+            junk, pr_int25 = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2), warp_param, percentiles=[0.25,0.75])
+            junk, pr_int95 = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2), warp_param, percentiles=[0.05,0.95])
+            junk, pr_int99 = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2), warp_param, percentiles=[0.01,0.99])
+            
             plt.fill_between(xx, pr_int25[:,0], pr_int25[:,1], alpha = 0.1,color=clr)
             plt.fill_between(xx, pr_int95[:,0], pr_int95[:,1], alpha = 0.1,color=clr)
             plt.fill_between(xx, pr_int99[:,0], pr_int99[:,1], alpha = 0.1,color=clr)
+            
+            # make the width of each line proportional to the epistemic uncertainty
+            junk, pr_int25l = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2-0.5*s2s), warp_param, percentiles=[0.25,0.75])
+            junk, pr_int95l = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2-0.5*s2s), warp_param, percentiles=[0.05,0.95])
+            junk, pr_int99l = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2-0.5*s2s), warp_param, percentiles=[0.01,0.99])
+            junk, pr_int25u = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2+0.5*s2s), warp_param, percentiles=[0.25,0.75])
+            junk, pr_int95u = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2+0.5*s2s), warp_param, percentiles=[0.05,0.95])
+            junk, pr_int99u = W.warp_predictions(np.squeeze(yhat), np.squeeze(s2+0.5*s2s), warp_param, percentiles=[0.01,0.99])    
+            plt.fill_between(xx, pr_int25l[:,0], pr_int25u[:,0], alpha = 0.3,color=clr)
+            plt.fill_between(xx, pr_int95l[:,0], pr_int95u[:,0], alpha = 0.3,color=clr)
+            plt.fill_between(xx, pr_int99l[:,0], pr_int99u[:,0], alpha = 0.3,color=clr)
+            plt.fill_between(xx, pr_int25l[:,1], pr_int25u[:,1], alpha = 0.3,color=clr)
+            plt.fill_between(xx, pr_int95l[:,1], pr_int95u[:,1], alpha = 0.3,color=clr)
+            plt.fill_between(xx, pr_int99l[:,1], pr_int99u[:,1], alpha = 0.3,color=clr)
+
+            # plot ceitle lines
             plt.plot(xx, pr_int25[:,0],color=clr, linewidth=0.5)
             plt.plot(xx, pr_int25[:,1],color=clr, linewidth=0.5)
             plt.plot(xx, pr_int95[:,0],color=clr, linewidth=0.5)
             plt.plot(xx, pr_int95[:,1],color=clr, linewidth=0.5)
             plt.plot(xx, pr_int99[:,0],color=clr, linewidth=0.5)
             plt.plot(xx, pr_int99[:,1],color=clr, linewidth=0.5)
-
+            
 
     if show_plot:
         plt.xlabel('Age')
         plt.ylabel(idp) 
         plt.title(idp)
         plt.xlim((0,90))
+        #plt.ylim((-1000,150000))
         plt.savefig(os.path.join(idp_dir, out_name, 'centiles_' + str(sex)),  bbox_inches='tight')
         plt.show()
      
